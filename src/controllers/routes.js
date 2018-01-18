766676ac3a95b80dfc83c1509530b644663c706f
mahmoud <m0597731363@gmail.com>
2018-01-18 16:51:50 +0200

    add sign up route #42

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const signup = require('./signup');
 const login = require('./login');
 
 router.get('/login', login.get);
 router.post('/loginuser', login.post);
+router.get('/signup', signup.get);
+router.post('/signup', signup.post);
+
 
 module.exports = router;
