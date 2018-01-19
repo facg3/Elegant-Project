ad1d6eabd08092bb0cdbc6e121cf43293afcb054
salam <salamdalloul93@gmail.com>
2018-01-19 16:51:04 +0200

    upload men outfits from database #24

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -5,12 +5,14 @@ const router = express.Router();
 const login = require('./login');
 const men = require('./men');
 const women = require('./women');
+const menOutfits = require('./menOutfits');
 
 
 router.get('/login', login.get);
 router.post('/loginuser', login.post);
 router.get('/men-fashion', men.get);
 router.get('/women-fashion', women.get);
+router.get('/men-outfits', menOutfits.get);
 
 
 module.exports = router;
---
+++ src/models/queries/viewMenOutfits.js
@@ -0,0 +1,14 @@
+const dbconnection = require('../database/db_connection');
+
+const viewMenOutfits = (cb) => {
+  const viewMenOutfitsSql = {
+    text: 'select * from outfits where gender = \'m\'',
+  };
+  dbconnection.query(viewMenOutfitsSql, (dataBaseConnectionError, menOutfits) => {
+    if (dataBaseConnectionError) return cb(dataBaseConnectionError);
+
+    return cb(null, menOutfits.rows);
+  });
+};
+
+module.exports = viewMenOutfits;
