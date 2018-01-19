0f1406f5e31f6a1a861f37a1af23144eafb1f555
salam <salamdalloul93@gmail.com>
2018-01-19 16:59:26 +0200

    upload wmen outfits from database #25

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -6,13 +6,13 @@ const login = require('./login');
 const men = require('./men');
 const women = require('./women');
 const menOutfits = require('./menOutfits');
-
+const womenOutfits = require('./womenOutfits');
 
 router.get('/login', login.get);
 router.post('/loginuser', login.post);
 router.get('/men-fashion', men.get);
 router.get('/women-fashion', women.get);
 router.get('/men-outfits', menOutfits.get);
-
+router.get('/women-outfits', womenOutfits.get);
 
 module.exports = router;
