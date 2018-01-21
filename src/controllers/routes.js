40ab242b4927ace9330616f2fa5276980c603fe5
mahmoud <m0597731363@gmail.com>
2018-01-21 09:44:28 +0200

    finish home page hbs #32

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const login = require('./login');
+const home = require('./home');
 
 router.get('/login', login.get);
 router.post('/loginuser', login.post);
-
+router.get('/',home.get)
 module.exports = router;
