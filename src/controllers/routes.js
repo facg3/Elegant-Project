56d635538274d25e3d2488eea5347a40fba13599
mahmoud <m0597731363@gmail.com>
2018-01-21 12:38:46 +0200

    styling and hbs the details page #34 #12

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
-
 const login = require('./login');
+const details = require('./details');
 
 router.get('/login', login.get);
 router.post('/loginuser', login.post);
+router.get('/details', details.get)
 
 module.exports = router;
