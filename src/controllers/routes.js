becacafba949af71bdc641f3f254b11c9f7ced0f
walaamedhat <nicedree@gmail.com>
2018-01-21 08:50:32 +0200

    logout #89

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const login = require('./login');
+const logout = require('./logout');
 
 router.get('/login', login.get);
 router.post('/loginuser', login.post);
+router.get('/logout', logout.get);
 
 module.exports = router;
