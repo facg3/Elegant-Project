974853eb9f4d209b57752d8120a69c156b7eec76
SalamDalloul <32265731+Salam-Dalloul@users.noreply.github.com>
2018-01-21 20:55:07 +0200

    Update viewWomenOutfits.js

--- src/models/queries/viewWomenOutfits.js
+++ src/models/queries/viewWomenOutfits.js
@@ -2,7 +2,7 @@ const dbconnection = require('../database/db_connection');
 
 const viewWomenOutfits = (cb) => {
   const viewWomenOutfitsSql = {
-    text: 'select * from outfits where gender = \'f\'',
+    text: 'select * from outfits where gender = \'women\'',
   };
   dbconnection.query(viewWomenOutfitsSql, (dataBaseConnectionError, womenOutfits) => {
     if (dataBaseConnectionError) return cb(dataBaseConnectionError);
