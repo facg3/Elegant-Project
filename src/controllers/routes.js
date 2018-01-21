92edb90c1cbc40cf39b55a4b5b1e828c7b29e42d
walaamedhat <nicedree@gmail.com>
2018-01-21 08:15:47 +0200

    hbs Contactus form and aboutus #35

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -3,8 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const login = require('./login');
+const aboutus = require('./aboutus');
+const contactus = require('./contactus');
 
 router.get('/login', login.get);
 router.post('/loginuser', login.post);
+router.get('/aboutus', aboutus.get);
+router.get('/contactus', contactus.get);
 
 module.exports = router;
