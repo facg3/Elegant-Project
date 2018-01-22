fcaf1e8d5ce58ac5321e818a500a61e39a93fded
Mahmoud MH <m0597731363@gmail.com>
2018-01-22 21:48:22 +0200

    Update blogs.js

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -1,9 +1,5 @@
 const view = require('../models/queries/viewBlogs.js');
-const container =[ {
-    'name1': "leftBigContainer", 'name2' : 'rightSmallContainer', 'name3' : 'rightSmallContainer' , 'name4' : 'leftBigContainer' } ]
 
-
-//,'topContainer2','rightSmallContainer','bottomContainer','rightSmallContainer','bottomContainer2','leftBigContainer'
 exports.get = (req, res) => {
   view.viewAllBlogs((dataBaseConnectionError, blog) =>{
     if (dataBaseConnectionError) return res.status(500).send({ error: dataBaseConnectionError });
